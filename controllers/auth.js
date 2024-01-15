1ba75e815b94813815e83c8ee83432d5c1ba605d
NaraeJang <j.naaaarae@gmail.com>
2024-01-15 15:08:52 -0500

    Before create hash password. I put the reason here why we can't put password as a string

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,10 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors/index");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-  // // How to check empty values in controller.
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError("Please provide name, email and password");
-  // }
-  // However, it works without checking empty values in controller. because we validate it in mongo db.
-
-  const user = await User.create({ ...req.body }); // !IMPORTANT! We also SAVED PASSWORD as a string which is very bad idea.
+  const user = await User.create({ ...req.body }); // !IMPORTANT! We also SAVED PASSWORD as a string which is very bad idea. If somebody breaks my app they can just get the password.
   res.status(StatusCodes.CREATED).json({ user });
 };
 
