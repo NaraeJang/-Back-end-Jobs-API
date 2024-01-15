0679805e36025b521077b2d49a20f3b16ab81959
NaraeJang <j.naaaarae@gmail.com>
2024-01-15 15:01:32 -0500

    Error Checking with MongoDB and Controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,17 @@
-const User = require("../models/User"); // 1. Import Model firstly.
-const { StatusCodes } = require("http-status-codes"); // 3. After confirm everything works, we import this function here.
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors/index");
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body }); // 4. create a variable to create User. Mongoose will validate the data.
-  res.status(StatusCodes.CREATED).json({ user }); // 2. Check if you can get the value in PostMan.
+  // // How to check empty values in controller.
+  // const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError("Please provide name, email and password");
+  // }
+  // However, it works without checking empty values in controller. because we validate it in mongo db.
+
+  const user = await User.create({ ...req.body }); // !IMPORTANT! We also SAVED PASSWORD as a string which is very bad idea.
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
