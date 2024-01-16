16ee444dec4e800b6c5f33902c8859f47a906fef
NaraeJang <j.naaaarae@gmail.com>
2024-01-15 20:04:16 -0500

    Logi nController Setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors/index");
+const { BadRequestError, UnauthenticatedError } = require("../errors/");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -11,7 +11,21 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("register user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email }); // Dont forget to put await
+
+  // compare password
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { register, login };
