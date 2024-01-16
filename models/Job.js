6ba348319e5548f6acf068d1dbae8af742e29d76
NaraeJang <j.naaaarae@gmail.com>
2024-01-16 16:25:52 -0500

    job Model

--- models/Job.js
+++ models/Job.js
@@ -1,5 +1,29 @@
 const mongoose = require("mongoose");
 
-const jobSchema = new mongoose.Schema({});
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId, // this is very very important. every time when we create a job, we assigned to one of our users.
+      ref: "User", // which model we are referencing.
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("job", jobSchema);
+module.exports = mongoose.model("Job", jobSchema);
