ef5565ab29fbe343dac920f64fe882cb86da31fa
NaraeJang <j.naaaarae@gmail.com>
2024-01-15 21:02:51 -0500

    My style Auth Middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authentication");
 
 const { login, register } = require("../controllers/auth");
 
 router.post("/register", register);
-router.post("/login", login);
+router.post("/login", authMiddleware, login);
 
 module.exports = router;
