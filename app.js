b6e092c8897b2db4dbfca6b1cd609c78eb16f8b8
NaraeJang <j.naaaarae@gmail.com>
2024-01-17 14:54:47 -0500

    Securty libraries setup

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// extract security packages
+const helmet = require("helmet"); // library that prevents numerous possible attacks by creating massive http headers.
+const cors = require("cors"); // (Cross Origin Resource Sharing) : to ensure our API is accessible from different domains.
+const xss = require("xss-clean"); // Sanitizes the user input in req.body, req.query and req.params. Protects us from cross scripting attacks where the attacker tries to inject some malicious code.
+const rateLimiter = require("express-rate-limit"); // limit the amount of requests the user can make.
+
 // connectDB
 const connectDB = require("./db/connect");
 const authMiddleware = require("./middleware/authentication");
@@ -14,8 +20,18 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// invoke security packages
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs.
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
